ad6d44783623138e9a75c1d34a16442a2699c40e
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2021-08-26 05:27:44 +0000

    Initial draft

---
+++ test/scripts/gen-parameters-db.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Copyright (c) 2021, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+#read arguments from stdin
+read parametersPath githubUserName testbranchName
+
+cat <<EOF > ${parametersPath}
+{
+    "\$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "_artifactsLocation": {
+            "value": "https://raw.githubusercontent.com/${githubUserName}/arm-oraclelinux-wls-admin/${testbranchName}/src/main/arm/"
+        },
+        "_artifactsLocationSasToken": {
+            "value": ""
+        },
+        "adminPasswordOrKey": {
+            "value": "GEN-UNIQUE"
+        },
+        "adminUsername": {
+            "value": "GEN-UNIQUE"
+        },
+        "databaseType": {
+            "value": "postgresql"
+        },
+        "dbPassword": {
+            "value": "GEN-UNIQUE"
+        },
+        "dbUser": {
+            "value": "GEN-UNIQUE"
+        },
+        "dsConnectionURL": {
+            "value": "GEN-UNIQUE"
+        },
+        "enableAAD": {
+            "value": false
+        },
+        "enableDB": {
+            "value": true
+        },
+        "jdbcDataSourceName": {
+            "value": "jdbc/postgresql"
+        },
+        "vmSizeSelect": {
+            "value": "Standard_D2as_v4"
+        },        
+        "wlsPassword": {
+            "value": "GEN-UNIQUE"
+        },
+        "wlsUserName": {
+            "value": "GEN-UNIQUE"
+        }
+    }
+}
+EOF
